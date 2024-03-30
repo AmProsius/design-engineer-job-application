bdf58ad708b0b4de6fb4443d3c53ae951053b8f1
fba <fba@cleverreach.com>
2024-03-30 22:18:01 +0100

    Remove unused dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import { cx } from "class-variance-authority";
+import { clsx } from "clsx";
 import type { Metadata } from "next";
 import { Inter, Roboto_Slab } from "next/font/google";
 import "./globals.css";
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <body
-        className={cx(
+        className={clsx(
           "flex min-h-dvh flex-col px-4 py-6 font-inter text-gray-600 lg:py-8 dark:bg-gray-900 dark:text-gray-400",
           inter.variable,
           robotoSlab.variable,
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [require("tailwindcss-react-aria-components")],
+  plugins: [],
 };
 
 export default config;
